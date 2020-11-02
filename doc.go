c87e2c3a1989c842e36560428f42fe53e0499ca3
Sindre Myren <sindre@searis.no>
2020-11-02 09:24:25 +0100

    doc: Slightly improve package docs

--- doc.go
+++ doc.go
@@ -1,11 +1,10 @@
 // Package jsoncat provide functions for concatenating JSON entities of the same
 // JSON type (object, array or string) while preserving the order of elements.
-// You can choose to conactinate JSON []byte slice directly with the Strings,
+// You can choose to conactinate JSON []byte slices directly with the Strings,
 // Arrrays ar Objects functions or you can pass in Go types to the Marshall...
-// functions as a convenience to marshalling each type yourself.
+// functions as a convenience to first marshal and then concatenate.
 //
 // Be aware that the package mostly expects the input JSON to be valid. We strip
-// leading and trailing white-space, and validates that we find the right
-// delimiters in each end of the JSON bytes, but we do not validate everything
-// in between.
+// leading and trailing white-space, and check for the right delimiters in each
+// end of the JSON bytes, but we do not validate anything in between.
 package jsoncat
